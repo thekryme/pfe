59e6eda69303dac92ed7536e2bcdac2d31608b78
thekryme <kevin.pressouyre@limayrac.fr>
2018-02-07 15:23:08 +0100

    init project

---
+++ alizee/src/PFEFrontBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace PFEFrontBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('PFEFrontBundle:Default:index.html.twig');
+    }
+}
---
+++ alizee/src/PFEFrontBundle/PFEFrontBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace PFEFrontBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PFEFrontBundle extends Bundle
+{
+}
